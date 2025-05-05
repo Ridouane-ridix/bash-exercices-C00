48c5fa224a1e0f0c28cabe2daf5547c90da374a3
Ridouane-ridix <ridix0202@gmail.com>
2025-05-05 15:42:42 -0400

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Ce script affiche la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS"
-
-date +"%Y-%m-%d %H:%M:%S"
-
+# Affiche la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS"
+echo "Date et heure actuelle : $(date '+%Y-%m-%d %H:%M:%S')"
