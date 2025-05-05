ecc3e07d5c83fc6ada3849ef218d2d83b0671617
Ridouane-ridix <ridix0202@gmail.com>
2025-05-05 19:41:16 -0400

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -5,13 +5,5 @@
 # Usage       : ./current_datetime.sh
 # Auteur      : Ridouane
 # ============================================================================
-
-# Vérifie qu'aucun argument n'est fourni
-if [ "$#" -ne 0 ]; then
-  echo "Ce script n'attend aucun argument."
-  echo "Usage : ./current_datetime.sh"
-  exit 1
-fi
-
 # Affiche la date et l'heure formatées
 echo "Date et heure actuelle : $(date '+%Y-%m-%d %H:%M:%S')"
