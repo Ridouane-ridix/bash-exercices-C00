736d57480094390380e82147aecf061afccfda48
Ridouane-ridix <ridix0202@gmail.com>
2025-05-05 15:21:36 -0400

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+# Vérifie l'existence d'un fichier
 
-read -p "Entrez le nom du fichier : " fichier
+read -p "Entrez le nom du fichier : " FILENAME
 
-if [ -e "$fichier" ]; then
-    echo "Le fichier '$fichier' existe."
-else
-    echo "Le fichier '$fichier' n'existe pas."
+if [ -z "$FILENAME" ]; then
+  echo "Erreur : nom du fichier vide."
+  exit 1
 fi
 
+if [ -f "$FILENAME" ]; then
+  echo "Le fichier '$FILENAME' existe"
+elif [ -d "$FILENAME" ]; then
+  echo "'$FILENAME' est un répertoire, pas un fichier"
+else
+  echo "Le fichier '$FILENAME' n'existe pas"
+fi
