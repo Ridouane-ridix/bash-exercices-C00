df2e725eaff156eff62f4724995807d9037184e4
Ridouane-ridix <ridix0202@gmail.com>
2025-05-05 19:47:47 -0400

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -8,19 +8,12 @@
 
 # Demande à l'utilisateur le nom du fichier
 read -p "Entrez le nom du fichier : " FILENAME
-
-# Vérifie si l'entrée est vide
-if [ -z "$FILENAME" ]; then
-  echo " Erreur : nom du fichier vide."
-  exit 1
-fi
-
 # Vérifie si c'est un fichier
 if [ -f "$FILENAME" ]; then
   echo " Le fichier '$FILENAME' existe."
 # Vérifie si c'est un répertoire
 elif [ -d "$FILENAME" ]; then
-  echo " '$FILENAME' est un répertoire, pas un fichier."
+  echo " '$FILENAME' est un repertoire, pas un fichier."
 # Sinon, le fichier n'existe pas
 else
   echo " Le fichier '$FILENAME' n'existe pas."
