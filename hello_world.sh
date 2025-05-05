8196f80af303ffffa402dabadc6001e59f4f43a6
Ridouane-ridix <ridix0202@gmail.com>
2025-05-05 19:40:28 -0400

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -4,14 +4,5 @@
 # Description : Affiche "Hello, World!" à l'écran
 # Usage       : ./hello_world.sh
 # Auteur      : Ridouane
-# ============================================================================
-
-# Vérifie qu'aucun argument n'est fourni
-if [ "$#" -ne 0 ]; then
-  echo "Ce script n'attend aucun argument."
-  echo "Usage : ./hello_world.sh"
-  exit 1
-fi
-
 # Affiche le message
 echo "Hello, World!"
