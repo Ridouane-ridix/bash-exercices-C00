26486fa86c1f74331b389c61606900b2b309e3a9
Ridouane-ridix <ridix0202@gmail.com>
2025-05-06 18:47:58 -0400

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -6,21 +6,4 @@
 # Auteur      : Ridouane
 # ============================================================================
 
-# Nom du repertoire à creer
-directory_name="test_directory"
-
-# Vérifie si le repertoire existe deja
-if [ -d "$directory_name" ]; then
-    echo "Le repertoire '$directory_name' existe deja."
-else
-    # Création du repertoire
-    mkdir "$directory_name"
-    
-    # Vérifie si la creation a réussi
-    if [ $? -eq 0 ]; then
-        echo " Le repertoire '$directory_name' a ete cree  avec succes."
-    else
-        echo " Erreur : Impossible de creer le repertoire '$directory_name'."
-        exit 1
-    fi
-fi
+mkdir -p test_directory
